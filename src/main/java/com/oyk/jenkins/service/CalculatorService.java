f39e577ddefebc5ae7b7802b24cf0553ee15be40
oyk0510 <youkyungoh0510@naver.com>
2025-05-21 12:13:03 +0900

    commit

---
+++ src/main/java/com/oyk/jenkins/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.oyk.jenkins.service;
+
+import com.oyk.jenkins.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
